2cd3fd28f4ab08dc416ba18e63afe461590752de
Rick <rikgao@icloud.com>
2015-10-11 21:24:05 -0400

    fixed reactive map issue

--- client/templates/site_view/site_view.js
+++ client/templates/site_view/site_view.js
@@ -4,18 +4,64 @@ Meteor.subscribe("sites");
 Meteor.subscribe("definitions");
 
 
+Template.site_view.onCreated(function() {
+
+  var self = this;
+
+
+  GoogleMaps.ready('site_view_map', function(map) {
+    // Adds the site marker once ready (already centered)
+
+    var marker;
+
+
+    self.autorun(function(){
+
+
+       var id = Router.current().params._site;
+    // // gets the url :_site
+      
+       var site_doc = Sites.findOne({siteID: id});
+
+      if(! site_doc){
+        console.log('something went wrong with mongo D:');
+      }
+
+      if(! marker){
+          
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(site_doc.Lat, site_doc.Lon),
+            zoom: site_doc.mapzoom,
+            map: map.instance
+
+
+        });
+
+      }
+      else{
+
+
+        marker.setPosition(new google.maps.LatLng(site_doc.Lat, site_doc.Lon));
+
+      }
+
+      map.instance.setCenter(marker.getPosition());
+
+
+    });
+
+
+  });
+
+
+
+});
+
 
 // Site View Helper
 Template.site_view.helpers({
-// get_url: function(){
-//     var id = Router.current().params._site;
-//     // gets the url :_site  
-//     var site_doc = Sites.findOne({siteID: id});
-//     console.log(site_doc.Lat);
 
-//     return Session.set({lat: site_doc.Lat, lng: site_doc.Lon});
 
-// },
 site_view_map_options: function() {
 
     var id = Router.current().params._site;
@@ -25,7 +71,7 @@ site_view_map_options: function() {
       // retrieves entire document that for the respective id
 
     // Makes sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
+    if (GoogleMaps.loaded() && id && site_doc) {
       // Map initialization options
 
 
@@ -41,29 +87,4 @@ site_view_map_options: function() {
    
   }
 
-// ,dictionary: function(desc){
-
-//  console.log(desc);
-//  return desc;
-
-
-
-// }
-
-});
-
-
-Template.site_view.onCreated(function() {
-
-  GoogleMaps.ready('site_view_map', function(map) {
-    // Adds the site marker once ready (already centered)
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
-  });
-});
-
-Template.site_view.onDestroyed(function() {
-  console.log('destroyed');
 });
